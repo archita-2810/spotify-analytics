3aee88f39e1708f9cd2f237f256894116386669e
archita-2810 <architasugandh@gmail.com>
2025-02-19 12:56:13 +0530

    added better background

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,17 @@ import Header from "@/components/navbar/header";
 
 export default function Home() {
   return (
-    <div className="font-poppins">
-      <Header />
+    <div className="font-poppins relative min-h-screen flex flex-col overflow-hidden">
+      <div className='relative z-20'>
+        <Header/>
+      </div>
+      <div className="absolute inset-0 overflow-hidden z-0">
+        <div className="blob left-[10%] top-[20%] animate-blob animation-delay-2000"></div>
+        <div className="blob right-[10%] bottom-[20%] animate-blob animation-delay-4000"></div>
+        <div className="blob left-[50%] top-[50%] animate-blob animation-delay-6000"></div>
+      </div>
+
+      <div className="absolute inset-0 bg-white/10 backdrop-blur-md border border-white/20 rounded-lg z-10"></div>
     </div>
   );
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import { transform } from 'next/dist/build/swc/generated-native';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -10,6 +12,33 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      animation: {
+        blob: "blob 12s infinite ease-in-out",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+            opacity: "0.8",
+          },
+          "25%": {
+            transform: "translate(-100px, 50px) scale(1.2)",
+            opacity: "0.7",
+          },
+          "50%": {
+            transform: "translate(100px, -50px) scale(0.9)",
+            opacity: "0.6",
+          },
+          "75%": {
+            transform: "translate(-50px, 100px) scale(1.1)",
+            opacity: "0.7",
+          },
+          "100%": {
+            transform: "translate(0px, 0px) scale(1)",
+            opacity: "0.8",
+          },
+        }
       },
       fontFamily: {
         poppins: ["poppins", "sans-serif"],
